5a1e97c17ef581ff1552efb0d687ec4c2b2067f1
Dianne De Jesus <djs.dianne@gmail.com>
2021-03-25 19:07:01 -0400

    added delete method and REST request tests

--- phonebook/server.js
+++ phonebook/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const phonebook = [
+let phonebook = [
     {
         id: 1,
         name: "Arto Hellas",
@@ -40,6 +40,13 @@ app.get('/api/persons/:id', (request, response) => {
     }else{
         response.status(404).end()
     }
+})
+
+app.delete('/api/person/:id', (request, response) => {
+    const id = Number(request.params.id)
+    phonebook = phonebook.filter(person => person.id !== id)
+
+    response.status(204).end()
 })
 
 const PORT = 3001
