c5700fc73bddd40cade921e08bf68a5c3fe75306
Dianne De Jesus <djs.dianne@gmail.com>
2021-03-31 11:10:45 -0400

    session

---
+++ daily-codewars/various1.js
@@ -0,0 +1,12 @@
+function sayHello(name) {
+  return 'Hello, ' + name
+}
+
+function correct(string)
+{
+	string = string.replace(/0/g, 'O')
+  string = string.replace(/5/g, 'S')
+  string = string.replace(/1/g, 'I')
+  
+  return string
+}
