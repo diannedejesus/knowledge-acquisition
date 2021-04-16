7c8ffab27f8e6bde7f528fa2837a033a91d3b3fa
Dianne De Jesus <djs.dianne@gmail.com>
2021-04-16 19:13:46 -0400

    todays session

---
+++ daily-codewars/various3.js
@@ -0,0 +1,15 @@
+function testEven(n) {
+  return n % 2 === 0 ? true : false;
+}
+
+function howManyLightsabersDoYouOwn(name="Jane") {
+  return name === 'Zach' ? 18 : 0
+}
+
+function move (position, roll) {
+ return roll*2 + position 
+  }
+
+  function getChar(c) {
+  return String.fromCharCode(c)
+}
