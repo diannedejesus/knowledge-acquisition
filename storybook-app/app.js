50f46db4a272e108c684ea3a4ef7d0be56697167
Dianne De Jesus <djs.dianne@gmail.com>
2021-04-24 12:31:59 -0400

    removed handlebar files and dependency

--- storybook-app/app.js
+++ storybook-app/app.js
@@ -3,8 +3,6 @@ const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
-const exphbs = require('express-handlebars')
-
 const methodOverride = require('method-override')
 const passport = require('passport')
 const session = require('express-session')
