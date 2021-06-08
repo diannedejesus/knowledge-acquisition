63f7f8988c4e4d71c6b6085f2edd02a6a1bfb954
Dianne De Jesus <djs.dianne@gmail.com>
2021-06-08 13:40:28 -0400

    todays session

--- daily-codewars/combined-string-methods.js
+++ daily-codewars/combined-string-methods.js
@@ -15,4 +15,20 @@ function array(arr){
       return check === '' ? null : check
     }
   }
+//#3
+function nameShuffler(str){
+    return str.split(' ').reverse().join(' ')
+  }
 
+//#4
+function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin){
+    //factor for shark beging able to bit you should be how far he is from you and not the pontoon
+    //but this one does not work in some cases if I do that.
+    
+    //let sharkDistanceFromYou = (sharkDistance - pontoonDistance)
+    let newSharkSpeed = (dolphin ? sharkSpeed / 2 : sharkSpeed)
+    let youReachPontoon = (pontoonDistance / youSpeed)
+    
+    //return (sharkDistanceFromYou / newSharkSpeed) < youReachPontoon ? 'Shark Bait!' : 'Alive!'
+    return (sharkDistance / newSharkSpeed) < youReachPontoon ? 'Shark Bait!' : 'Alive!'
+}
