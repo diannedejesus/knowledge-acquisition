2139d0e7a3396a716c1bc9217af4624bbd8eac90
Dianne De Jesus <djs.dianne@gmail.com>
2021-07-05 08:17:26 -0400

    another excersize

--- daily-codewars/jul05-21.js
+++ daily-codewars/jul05-21.js
@@ -2,3 +2,20 @@ function formatPoem(poem) {
   return poem.split('. ').join('.\n') 
 }
 
+//2
+function sumFromString(str){
+  let results = [0,'']
+
+  for(let i=0; i<str.length; i++){ 
+    if(Number(str[i]) > -1 && Number(str[i+1]) > -1){
+      results[1] += str[i]
+    }else if(Number(str[i]) > -1){
+      results[0] += Number(results[1]+str[i])
+      results[1] = ''
+    }
+  }
+  return results[0]
+}
+
+
+    
