fde663507969e8a97b406d5bd2a67272d8b8a9ad
Dianne De Jesus <djs.dianne@gmail.com>
2021-07-07 07:32:49 -0400

    fixed

--- daily-codewars/jul07-21.js
+++ daily-codewars/jul07-21.js
@@ -1,8 +1,5 @@
 function howMuchILoveYou(nbPetals) {
-    const phrase = ['I love you', 'a little', 'a lot', 'passionately', 'madly', 'not at all']
-    if(nbPetals > 6){
-      return phrase[(nbPetals % 6)-1]
-    }else{
-      return phrase[nbPetals-1]
-    }
+    const phrase = ['not at all', 'I love you', 'a little', 'a lot', 'passionately', 'madly']
+    
+    return phrase[(nbPetals % 6)]
 }
