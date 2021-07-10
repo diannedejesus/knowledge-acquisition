c49bc249230c95b3df53ad9ab59d0c3c5c2d3bf8
Dianne De Jesus <djs.dianne@gmail.com>
2021-07-10 08:04:01 -0400

    another

--- daily-codewars/jul10-21.js
+++ daily-codewars/jul10-21.js
@@ -1,3 +1,9 @@
 function all( arr, fun ){
   return arr.every(el => fun(el))
 }
+
+//2
+function first(arr, n) {
+    n = n || n === 0 ? n : 1
+    return arr.filter((el, i) => { if(i<n) return el})
+  }
