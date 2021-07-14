07f180d964063999950ec9df364a4070419329f9
Dianne De Jesus <djs.dianne@gmail.com>
2021-07-14 06:48:06 -0400

    alternate added

--- daily-codewars/jul14-21.js
+++ daily-codewars/jul14-21.js
@@ -4,3 +4,20 @@ var gimme = function (inputArray) {
   
   return inputArray.indexOf(sortedArray[1])
 };
+
+//in this second one they find the max and min value of the array
+//then the loop through the array to find the index of the value 
+//that does not match either value.
+
+var gimme = function (inputArray) {
+    let index = 0;
+    let max = Math.max(...inputArray);
+    let min = Math.min(...inputArray);
+    for(let i = 0; i < inputArray.length; i++){
+      if(inputArray[i] !== max && inputArray[i] !== min){        
+        index = i;
+      }
+    }
+    return index;
+  };
+
