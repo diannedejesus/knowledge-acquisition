11ca134bbf03035a9bf4046d66a5359cda911f2e
Dianne De Jesus <djs.dianne@gmail.com>
2021-07-15 06:59:01 -0400

    another

--- daily-codewars/jul15-21.js
+++ daily-codewars/jul15-21.js
@@ -1,3 +1,14 @@
 function getEvenNumbers(numbersArray){
   return numbersArray.filter(el => el % 2 === 0)
 }
+
+//
+function min(arr, toReturn) {
+    let minValue = Math.min(...arr)
+  
+    if(toReturn === 'value'){
+      return minValue
+    }else{
+      return arr.indexOf(minValue)
+    }
+  }
