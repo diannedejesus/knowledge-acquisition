696b5ac2e8c42d0dbb0100b2bcb1dabadd98b802
Dianne De Jesus <djs.dianne@gmail.com>
2021-08-06 07:09:02 -0400

    todays session

---
+++ daily-codewars/eloquent03.js
@@ -0,0 +1,86 @@
+// A list
+// Objects, as generic blobs of values, can be used to build all sorts of data structures. A common data structure is the list (not to be confused with array). A list is a nested set of objects, with the first object holding a reference to the second, the second to the third, and so on.
+
+// let list = {
+//   value: 1,
+//   rest: {
+//     value: 2,
+//     rest: {
+//       value: 3,
+//       rest: null
+//     }
+//   }
+// };
+// The resulting objects form a chain, like this:
+
+// A linked list
+// A nice thing about lists is that they can share parts of their structure. For example, if I create two new values {value: 0, rest: list} and {value: -1, rest: list} (with list referring to the binding defined earlier), they are both independent lists, but they share the structure that makes up their last three elements. The original list is also still a valid three-element list.
+
+// Write a function arrayToList that builds up a list structure like the one shown when given [1, 2, 3] as argument. Also write a listToArray function that produces an array from a list. Then add a helper function prepend, which takes an element and a list and creates a new list that adds the element to the front of the input list, and nth, which takes a list and a number and returns the element at the given position in the list (with zero referring to the first element) or undefined when there is no such element.
+
+// If you haven’t already, also write a recursive version of nth.
+
+// // Your code here.
+function arrayToList(arr){
+
+    let list = {}
+    let currentPlace = list
+
+    currentPlace.value = arr[0]
+    currentPlace.rest = null
+
+    for(let i=1; i<arr.length; i++){
+        currentPlace.rest = {value: arr[i]}
+        currentPlace.rest.rest = null
+        currentPlace = currentPlace.rest
+    }
+    
+    return list
+}
+
+function listToArray(list){
+    let array = []
+    let currentPlace = list
+
+    while(currentPlace){
+        array.push(currentPlace.value)
+        currentPlace = currentPlace.rest
+    }
+    
+    return array
+}
+
+function prepend(element, list){
+    let newList = {value: element, rest: list}
+    return newList
+}
+
+// function nth(list, item){
+//     let currentPlace = list
+
+//     for(let i=0; i<item; i++){
+//         currentPlace = currentPlace.rest
+//     }
+
+//     return currentPlace.value
+// }
+
+function nth(list, item){  //recursive
+    //let currentPlace = list
+
+    if(item > 0){
+        return nth(list.rest, item-1)
+    }else{
+        return list.value
+    }
+
+    //return currentPlace.value
+}
+ console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30])));
+// // → [10, 20, 30]
+ console.log(prepend(10, prepend(20, null)));
+// // → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// // → 20
