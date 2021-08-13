8943d0c4715e1ab814c48e0813de8b896454f6a2
Dianne De Jesus <djs.dianne@gmail.com>
2021-08-13 19:17:47 -0400

    todays session

--- daily-codewars/eloquent05.js
+++ daily-codewars/eloquent05.js
@@ -51,3 +51,17 @@ console.log(every([2, 4, 16], n => n < 10));
 // → false
 console.log(every([], n => n < 10));
 // → true
+
+// Dominant writing direction
+// Write a function that computes the dominant writing direction in a string of text. Remember that each script object has a direction property that can be "ltr" (left to right), "rtl" (right to left), or "ttb" (top to bottom).
+
+// The dominant direction is the direction of a majority of the characters that have a script associated with them. The characterScript and countBy functions defined earlier in the chapter are probably useful here.
+
+function dominantDirection(text) {
+  // Your code here.
+}
+
+console.log(dominantDirection("Hello!"));
+// → ltr
+console.log(dominantDirection("Hey, مساء الخير"));
+// → rtl
