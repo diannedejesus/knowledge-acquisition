4f0de42b16a39caf5d00251fded5da96181f8a53
Dianne De Jesus <djs.dianne@gmail.com>
2021-10-07 20:18:42 -0400

    this session

---
+++ daily-codewars/oct07-21.js
@@ -0,0 +1,16 @@
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
+
+//it used the wrong symbol for assignment
+
+//--------------------------------------------------
+
+function divide(weight){
+  if(weight < 3){ return false}
+  if (weight % 2 === 0){
+    return true
+  }else{
+    return false
+  }
+}
